50eda4bf21a381e0d28c95db339564150e8cc1b8
Anderson-431 <75143329+Anderson-431@users.noreply.github.com>
2025-06-12 14:58:47 -0300

    Update category

--- backend2/src/main/java/com/aspdeveloper/dscatalog2/services/CategoryService.java
+++ backend2/src/main/java/com/aspdeveloper/dscatalog2/services/CategoryService.java
@@ -11,28 +11,30 @@ import org.springframework.transaction.annotation.Transactional;
 import com.aspdeveloper.dscatalog2.dto.CategoryDTO;
 import com.aspdeveloper.dscatalog2.entities.Category;
 import com.aspdeveloper.dscatalog2.repositories.CategoryRepository;
-import com.aspdeveloper.dscatalog2.services.exceptions.EntityNotFoundException;
+import com.aspdeveloper.dscatalog2.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> list = repository.findAll(); 
-		
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada"));
 		return new CategoryDTO(entity);
 	}
-	
+
 	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
@@ -41,4 +43,15 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id não encontrado" + id);
+		}
+	}
 }
