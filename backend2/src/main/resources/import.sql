0ac94145213bb75863eb56461924acd3d609b177
Anderson-431 <75143329+Anderson-431@users.noreply.github.com>
2025-06-16 21:25:36 -0300

    Dados de auditoria

--- backend2/src/main/resources/import.sql
+++ backend2/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb2_category(name) VALUES ('Livros');
-INSERT INTO tb2_category(name) VALUES ('Eletrônicos');
-INSERT INTO tb2_category(name) VALUES ('Computadores');
+INSERT INTO tb2_category(name, created_At) VALUES ('Livros', NOW());
+INSERT INTO tb2_category(name, created_At) VALUES ('Eletrônicos', NOW());
+INSERT INTO tb2_category(name, created_At) VALUES ('Computadores', NOW());
